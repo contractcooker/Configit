1ee227aef4cb8c37ad6c5f83cf99987b24efbabe
Thomas Barron <barron.tom@gmail.com>
2022-03-11 15:09:53 -0500

    Hello World

---
+++ Configit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Configit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
