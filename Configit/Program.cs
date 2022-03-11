de860236df4867f86ef190ef7931fcb731483d99
Thomas Barron <barron.tom@gmail.com>
2022-03-11 15:11:10 -0500

    Read from file.

--- Configit/Program.cs
+++ Configit/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.IO;
 
-namespace Configit
+namespace Resolve_dependencies
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            string filepath = @"/Users/husker/Downloads/testdata/";
+            string filename;
+            for (int i = 0; i < 10; i++)
+            {
+                filename = filepath + "input00" + i + ".txt";
+                Console.WriteLine(filename);
+                var lines = File.ReadAllLines(filename);
+                foreach (var line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            
         }
     }
 }
