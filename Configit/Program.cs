480b49dbbda9bf2ffabb04c161edd6f8ccd21ce8
Thomas Barron <barron.tom@gmail.com>
2022-03-12 19:00:40 -0500

    Fixing output

--- Configit/Program.cs
+++ Configit/Program.cs
@@ -8,71 +8,75 @@ namespace Resolve_dependencies
     {
         static void Main(string[] args)
         {
-            string inputPath = @"/Users/husker/RiderProjects/Configit/input/";
             bool pass = true;
 
             try
             {
-                var inputFiles = Directory.EnumerateFiles(inputPath, "input*.txt");
-                foreach (string currentFile in inputFiles)
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            string inputPath = @"/Users/husker/RiderProjects/Configit/input/";
+            string outputPath = @"/Users/husker/RiderProjects/Configit/output/";
+
+            var inputFiles = Directory.EnumerateFiles(inputPath, "input*.txt");
+            foreach (string currentFile in inputFiles)
+            {
+                string outputFile = currentFile.Replace("input/input", "output/output");
+
+                Console.WriteLine(outputFile);
+                Console.WriteLine(currentFile);
+                var lines = File.ReadAllLines(currentFile);
+                int numPackages = int.Parse(lines[0]);
+
+                Console.WriteLine($"Number of packages: {numPackages}");
+
+                Dictionary<string, string> packageToVersionDictionary = new Dictionary<string, string>();
+                for (int j = 1; j <= numPackages; j++)
                 {
-                    string outputFile = currentFile.Replace("input/input", "output/output");
-                    
-                    Console.WriteLine(outputFile);
-                    Console.WriteLine(currentFile);
-                    var lines = File.ReadAllLines(currentFile);
-                    int numPackages = int.Parse(lines[0]);
+                    var subs = lines[j].Split(',');
 
-                    Console.WriteLine($"Number of packages: {numPackages}");
+                    pass = packageToVersionDictionary.TryAdd(subs[0], subs[1]);
+                    if (!pass) break;
+                }
 
-                    Dictionary<string, string> packageToVersionDictionary = new Dictionary<string, string>();
-                    for (int j = 1; j <= numPackages; j++)
+                foreach (var kvp in packageToVersionDictionary)
+                {
+                    Console.WriteLine($"Package: {kvp.Key}");
+                    Console.WriteLine($"Version: {kvp.Value}");
+                }
+
+                if (pass)
+                {
+                    bool hasDependencies = lines.Length > 1 + numPackages;
+                    if (hasDependencies)
                     {
-                        var subs = lines[j].Split(',');
-
-                        pass = packageToVersionDictionary.TryAdd(subs[0], subs[1]);
-                        if (!pass) break;
-                    }
-
-                    foreach (var kvp in packageToVersionDictionary)
-                    {
-                        Console.WriteLine($"Package: {kvp.Key}");
-                        Console.WriteLine($"Version: {kvp.Value}");
-                    }
-
-                    if (pass)
-                    {
-                        bool hasDependencies = lines.Length > 1 + numPackages;
-                        if (hasDependencies)
+                        for (int j = 2 + numPackages; j < lines.Length; j++)
                         {
-                            for (int j = 2 + numPackages; j < lines.Length; j++)
-                            {
-                                if (!pass) break;
-                                var subs = lines[j].Split(',');
-                                int numDependencies = subs.Length / 2 - 1;
+                            if (!pass) break;
+                            var subs = lines[j].Split(',');
+                            int numDependencies = subs.Length / 2 - 1;
 
-                                if (packageToVersionDictionary.ContainsKey(subs[0]))
+                            if (packageToVersionDictionary.ContainsKey(subs[0]))
+                            {
+                                if (packageToVersionDictionary[subs[0]] == subs[1])
                                 {
-                                    if (packageToVersionDictionary[subs[0]] == subs[1])
+                                    for (int k = 0; k < 2 * numDependencies; k += 2)
                                     {
-                                        for (int k = 0; k < 2 * numDependencies; k += 2)
-                                        {
-                                            if (packageToVersionDictionary.ContainsKey(subs[k + 2]) &&
-                                                packageToVersionDictionary[subs[k + 2]] == subs[k + 3])
-                                                continue;
-                                            pass = packageToVersionDictionary.TryAdd(subs[k + 2], subs[k + 3]);
-                                        }
+                                        if (packageToVersionDictionary.ContainsKey(subs[k + 2]) &&
+                                            packageToVersionDictionary[subs[k + 2]] == subs[k + 3])
+                                            continue;
+                                        pass = packageToVersionDictionary.TryAdd(subs[k + 2], subs[k + 3]);
                                     }
                                 }
                             }
                         }
                     }
-                    File.WriteAllText(outputFile, pass ? "PASS" : "FAIL");
                 }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
+                Directory.CreateDirectory(outputPath);
+                File.WriteAllText(outputFile, pass ? "PASS" : "FAIL");
             }
         }
     }
