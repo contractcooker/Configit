f01d1141916d4dd6fee60ee5880181ee12aa878f
Thomas Barron <barron.tom@gmail.com>
2022-03-14 09:32:57 -0400

    Added initial tests

---
+++ Configit.Tests/FileHelperTests.cs
@@ -0,0 +1,33 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Configit.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        internal FileHelper _fileHelper;
+
+        [TestInitialize]
+        public void Init()
+        {
+            
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullDependency_ThrowsArgumentNullException()
+        {
+            FileHelper fh = new FileHelper(null);
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EmptyDependency_ThrowsArgumentNullException()
+        {
+            FileHelper fh = new FileHelper("");
+        }
+        
+    }
+}
