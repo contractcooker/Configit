dbe586172f3a3c9f399e989549e508bf34ee96d6
Thomas Barron <barron.tom@gmail.com>
2022-03-14 09:35:40 -0400

    Adding IoHelperTests.cs

---
+++ Configit.Tests/IoHelperTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Configit.Tests
+{
+    [TestClass]
+    public class IoHelperTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            
+        }
+    }
+}
